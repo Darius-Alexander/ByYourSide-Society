3f27a4658d3a75ef09412205a808d086a817d6f5
Darius Alexander <dariusalexander1118@gmail.com>
2025-12-25 14:35:11 -0800

    File/Folder structure set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+// Root component of the React application
+// Top level component for entire website
+
+// Defines a functional component named App using RAFCE
+
+import React from 'react';
+import { Footer, Blog, Header, Possibility, Features, Byyourside } from './containers';
+import { CTA, Brand, Navbar } from './Components';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Byyourside />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Entry point of the React application, connects to browser
+// Responsible for rendering the root component into the DOM
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root'); // Grabs DOM element with id 'root' from index.html
+const root = createRoot(container); // creates a React root attached to the DOM container
+root.render(<App />); // tells React to render the App component into root
