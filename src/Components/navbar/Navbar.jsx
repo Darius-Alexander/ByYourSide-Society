2a07b4d5a12a3601420fd368c6b1976ad6b7d4ad
Darius Alexander <dariusalexander1118@gmail.com>
2025-12-27 01:00:01 -0800

    navbar.jsx + navbar.css

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="byyourside_navbar">
+      <div className="byyourside_navbar-links">
+        <div className="byyourside_navbar-links_logo">
+          <img src={logo} alt="logo"/>
+        </div>
+        <div className="byyourside_navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#about">About</a></p>
+          <p><a href="#initiatives">Initiatives</a></p>
+          <p><a href="#partners">Partners</a></p>
+          <p><a href="#team">Team</a></p>
+        </div>
+      </div>
+      <div className="byyourside_navbar-donate">
+        <button type="button">Donate!</button>
+      </div>
     </div>
   )
 }
