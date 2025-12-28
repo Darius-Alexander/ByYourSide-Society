1e55f151e8d35edc22a2d08fd712c01f0f36de16
Darius Alexander <dariusalexander1118@gmail.com>
2025-12-27 19:01:27 -0800

    Initial header styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './header.css'
+import ByYourSide from '../../assets/logo.svg'
+
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="byyourside_header section_padding" id="home">
+      <div className ="byyourside_header-content">
+        <h1 className="gradient_text">Assisting and Empowering for a Brighter Tomorrow</h1>
+        <p> ByYourSide Society is a youth-led compassionate initiative dedicated 
+            to uplifting the less fortunate in Vancouver and the lower mainland. Committed
+            to fostering positive transformation, it brings hope, support, and change to 
+            the local community, embodying solidarity and progress for all.
+        </p>
+        <div className="byyourside_header-content_input">
+          <input type="email" placeholder="Enter your email" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="byyourside_header-image">
+          <img src={ByYourSide} alt="ByYourSide Society" />
+        </div>
+      </div>
     </div>
   )
 }
