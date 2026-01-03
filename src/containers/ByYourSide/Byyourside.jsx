372c263b46b41ad1afba25547e5f620590c86d34
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-03 14:57:56 -0800

    Byyourside structure initialized

--- src/containers/ByYourSide/Byyourside.jsx
+++ src/containers/ByYourSide/Byyourside.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './byyourside.css'
+import { Feature } from '../../Components'
+
 
 const Byyourside = () => {
   return (
-    <div>
-      byyourside
+    <div className="byyourside_byyourside section_margin" id="byyourside">
+      <div className="byyourside_byyourside-feature">
+          <Feature />
+      </div>
+      <div className= "byyourside_byyourside-heading">
+        <h1 className = "gradient_text">
+          Empowering Youth, Transforming Communities
+        </h1>
+        <p>
+          Discover the impact of ByYourSide Society's youth-led initiatives in uplifting Vancouver's less fortunate. Join us in fostering hope, support, and positive change for a brighter tomorrow.
+        </p>
+      </div>
+      <div className="byyourside_byyourside-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
