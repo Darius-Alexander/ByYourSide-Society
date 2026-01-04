a951873cf5c8bca8977a05a2f2e42c1a33f51c96
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-03 18:06:37 -0800

    props added Byyourside

--- src/containers/ByYourSide/Byyourside.jsx
+++ src/containers/ByYourSide/Byyourside.jsx
@@ -7,7 +7,11 @@ const Byyourside = () => {
   return (
     <div className="byyourside_byyourside section_margin" id="byyourside">
       <div className="byyourside_byyourside-feature">
-          <Feature />
+          <Feature 
+            title="Our initiatives" 
+            text="ByYourSide Society runs several iniatives aimed towards uplifting the lives of the less
+                  fortunate around the Greater Vancouver Area highlighted by Operation Hunger and Project Warmth."
+          />
       </div>
       <div className= "byyourside_byyourside-heading">
         <h1 className = "gradient_text">
@@ -18,9 +22,18 @@ const Byyourside = () => {
         </p>
       </div>
       <div className="byyourside_byyourside-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature 
+          title="Operation Hunger" 
+          text="Operation Hunger is an initiative focused on providing meals to those in need. Through 
+                fundraisers such as Donut Sales and Car Washes, BYSS commits to a monthly donation of 
+                $100 to the greater Vancoouver Food Bank"
+        />
+        <Feature 
+          title="Project Warmth" 
+          text="Project Warmth is an initiative aimed at providing the needy with warm, adequate clothing. 
+                Each year BYSS donates hundreds of gently used clothing to the Big Brothers of Vancouver
+                through hosting large scale clothing drives"
+        />
       </div>
     </div>
   )
