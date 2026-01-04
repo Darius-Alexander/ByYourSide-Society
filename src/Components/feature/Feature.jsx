36b1792f4b4c4dd3a4cbe1dbd085855dfb329cec
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-03 17:52:24 -0800

    Feature component initialized

--- src/Components/feature/Feature.jsx
+++ src/Components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>
-      feature
+    <div className="byyourside_features-container_feature"> 
+      <div className="byyourside_features-container_feature-title">
+        <div />
+        <h1> {title} </h1>
+      </div>
+      <div classNamwe="byyourside_features-container_feature-text">
+        {text}
+      </div>
     </div>
   )
 }
