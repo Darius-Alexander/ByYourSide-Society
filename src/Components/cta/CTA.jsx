14438fdc53b9e0c8810685537e62f3dde380180e
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-05 15:15:52 -0800

    CTA componet initialized

--- src/Components/cta/CTA.jsx
+++ src/Components/cta/CTA.jsx
@@ -3,8 +3,18 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="byyourside_cta">
+      <div className="byyourside_cta-content">
+        <p>
+          Donate Now and Start Receiving
+        </p>
+        <h3>
+          Your Monthly Impact Today!
+        </h3>
+        <div className="byyourside_cta-button">
+          <button type="button">Donate!</button>
+        </div>
+      </div>
     </div>
   )
 }
