23340ded82df01beaf7f8993493d06cdc9694306
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-06 17:19:43 -0800

    Blog component initialized

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './blog.css'
+import { Article } from '../../components';
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className="byyourside_blog section_padding" id="blog">
+      <div className="byyourside_blog-heading">
+        <h1 className="gradient_text"> 
+          Photos from some of our favourite events!
+        </h1>
+      </div>
+      <div className="byyourside_blog-container">
+        <div className="byyourside_blog-container_groupA">
+          <Article />
+        </div>
+        <div className="byyourside_blog-container_groupB">
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
     </div>
   )
 }
