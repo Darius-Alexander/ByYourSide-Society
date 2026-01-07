cc0a0fd66436d1d5c9ce94070a9479e7f78aa47c
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-06 17:30:39 -0800

    imports.js for blog page made

--- src/Components/article/Article.jsx
+++ src/Components/article/Article.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl }) => {
   return (
-    <div>
-      article
+    <div className="byyourside_blog-container_article">
+      <div className="byyourside_blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
     </div>
   )
 }
