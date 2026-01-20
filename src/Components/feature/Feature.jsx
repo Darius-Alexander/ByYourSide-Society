d5a177616f2e045e351b2720c75c588d83a98278
Darius Alexander <dariusalexander1118@gmail.com>
2026-01-19 16:31:48 -0800

    feature object changed, image prop instead of  p

--- src/Components/feature/Feature.jsx
+++ src/Components/feature/Feature.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = ({ title, text }) => {
+const Feature = ({ title, imgUrl }) => {
   return (
     <div className="byyourside_features-container_feature"> 
       <div className="byyourside_features-container_feature-title">
         <div />
         <h1> {title} </h1>
       </div>
-      <div className="byyourside_features-container_feature-text">
-        <p> {text} </p>
+      <div className="byyourside_features-container_feature-image">
+        <img src={imgUrl} alt={title} />
       </div>
     </div>
   )
