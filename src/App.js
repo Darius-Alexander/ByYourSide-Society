4de95cc57b4f99727f98821f2a7f641e9f03b348
Darius Alexander <dariusalexander1118@gmail.com>
2026-02-04 16:00:53 -0800

    App.js reconfigured

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 // Root component of the React application
 // Top level component for entire website
 
-// Defines a functional component named App using RAFCE
-
 import React from 'react';
+
+// React Router imports for page navigation
+// BrowserRouter: Wraps the app to enable routing
+// Routes: Container that holds all Route definitions
+// Route: Defines a URL path and which component to show
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import page components (these are what Routes will switch between)
 import { Home, Donation } from './containers';
+
+// Navbar stays here because it appears on every page
 import { Navbar } from './Components';
+
 import './app.css';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
+        {/* Navbar is OUTSIDE Routes so it shows on every page */}
         <div className="gradient_bg">
           <Navbar />
         </div>
         <Routes>
+          {/* When URL is "/", show the Home component */}
           <Route path="/" element={<Home />} />
+          {/* When URL is "/donate", show the Donation component */}
           <Route path="/donate" element={<Donation />} />
         </Routes>
       </div>
     </BrowserRouter>
   )
 }
-
 export default App
--- src/containers/index.js
+++ src/containers/index.js
@@ -6,5 +6,7 @@ export { default as Footer} from './footer/Footer';
 export { default as Header} from './header/Header';
 export { default as Possibility} from './possibility/Possibility';
 export { default as Byyourside} from './ByYourSide/Byyourside';
+
+// Page components for React Router
 export { default as Donation} from './donation/Donation';
 export { default as Home} from './home/Home';
