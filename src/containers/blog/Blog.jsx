a371a5a6fa7f6c66502ed0a1a70d20c7c5bed0ac
Darius Alexander <dariusalexander1118@gmail.com>
2026-02-13 12:52:41 -0800

    blog section updated + reformatted

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './blog.css'
 import { Article } from '../../Components';
-import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import { blog01, blog02, blog03, blog04, blog05, blog06 } from './imports';
 
 const Blog = () => {
   return (
@@ -12,15 +12,12 @@ const Blog = () => {
         </h1>
       </div>
       <div className="byyourside_blog-container">
-        <div className="byyourside_blog-container_groupA">
-          <Article imgUrl = { blog01 } date="Sep 26, 2021" title="Event: Car Wash Fundraiser"/>
-        </div>
-        <div className="byyourside_blog-container_groupB">
-          <Article imgUrl = { blog02 } date="Sep 26, 2021" title="Event: Car Wash Fundraiser"/>
-          <Article imgUrl = { blog03 } date="Sep 26, 2021" title="Event: Car Wash Fundraiser"/>
-          <Article imgUrl = { blog04 } date="Sep 26, 2021" title="Event: Car Wash Fundraiser"/>
-          <Article imgUrl = { blog05 } date="Sep 26, 2021" title="Event: Car Wash Fundraiser"/>
-        </div>
+        <Article imgUrl={blog06} date="Dec 30, 2026" title="Operation Hunger: KrispyKreme Fundraiser - Burnaby, BC"/>
+        <Article imgUrl={blog03} date="Dec 30, 2026" title="Operation Hunger: KrispyKreme Fundraiser - Richmond, BC"/>
+        <Article imgUrl={blog04} date="Feb 23, 2024" title="Project Warmth: Big Brothers Clothing Drive - Vancouver, BC"/>
+        <Article imgUrl={blog05} date="Jan 15, 2022" title="Operation Hunger: KrispyKreme Fundraiser - Burnaby, BC"/>
+        <Article imgUrl={blog02} date="Nov 26, 2023" title="Operation Hunger: KrispyKreme Fundraiser - Vancouver, BC"/>
+        <Article imgUrl={blog01} date="Oct 1, 2023" title="Operation Hunger: P&D Car Wash Fundraiser - Vancouver, BC"/>
       </div>
     </div>
   )
--- src/containers/blog/imports.js
+++ src/containers/blog/imports.js
@@ -3,6 +3,7 @@ import blog02 from '../../assets/blog02.jpg';
 import blog03 from '../../assets/blog03.jpg';
 import blog04 from '../../assets/blog04.jpg';
 import blog05 from '../../assets/blog05.jpg';
+import blog06 from '../../assets/blog06.jpg';
 
 export {
     blog01,
@@ -10,4 +11,5 @@ export {
     blog03,
     blog04,
     blog05,
+    blog06,
 }
